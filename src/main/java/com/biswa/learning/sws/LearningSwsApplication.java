390b86778ea5f892221b792ecb3c97a4206089e9
biswaranjan-1984 <biswaranjan.001@gmail.com>
2021-08-02 21:07:13 +0100

    First commit

---
+++ src/main/java/com/biswa/learning/sws/LearningSwsApplication.java
@@ -0,0 +1,13 @@
+package com.biswa.learning.sws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biswa/learning/sws/LearningSwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biswa.learning.sws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
